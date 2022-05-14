ff47f5ad5b7e3a4d91a72e2294809ec29b5c8ed3
Nick <maniutin@gmail.com>
2022-05-13 22:11:41 -0400

    implement local storage

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ function App() {
   const [inputValue, setInputValue] = useState("");
   const [enteredPrompt, setEnteredPrompt] = useState("");
   const [returnedText, setReturnedText] = useState("");
-  const [pastPrompts, setPastPrompts] = useState([]);
+  const [pastPrompts, setPastPrompts] = useState(() => {
+    const saved = localStorage.getItem("recent-prompts");
+    const initialValue = JSON.parse(saved);
+    return initialValue || [];
+  });
 
   useEffect(() => {
     let promptsArr = [...pastPrompts];
--- src/components/ResultListContainer.js
+++ src/components/ResultListContainer.js
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
+
 import ResultList from "./ResultList";
 
 export default function ResultListContainer(props) {
   const { returnedText, enteredPrompt, pastPrompts } = props;
+
+  useEffect(() => {
+    localStorage.setItem("recent-prompts", JSON.stringify(pastPrompts));
+  }, [pastPrompts]);
 
   return (
     <div className="result-list">
