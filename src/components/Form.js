f3f9558e37f0b4d7188c16f1935beee4f2fbb71f
Nick <maniutin@gmail.com>
2022-05-14 21:00:23 -0400

    style form

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,21 +7,16 @@ export default function Form(props) {
 
   return (
     <div className="input-form">
-      <h2>Enter prompt:</h2>
       <form onSubmit={handleSubmit}>
         <label htmlFor="input-field" />
         <textarea
           id="input-field"
           name="input-field"
-          rows="4"
-          cols="50"
+          placeholder="Enter prompt:"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
           required
-        >
-          At w3schools.com you will learn how to make a website. They offer free
-          tutorials in all web development technologies.
-        </textarea>
+        />
         <input type="submit" value="Submit" />
       </form>
     </div>
